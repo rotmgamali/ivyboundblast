c7dbde3c1e86a5515fd85e123add676c8bc0cbc2
Your Name <you@example.com>
2026-03-09 23:51:58 +0700

    chore: final cleanup of legacy lead generation and local scripts

--- automation_scrapers/__init__.py
+++
@@ -1,4 +0,0 @@
-# Scrapers package
-from .school_scraper import scrape_school_website, scrape_website_text, format_for_ai
-
-__all__ = ['scrape_school_website', 'scrape_website_text', 'format_for_ai']
